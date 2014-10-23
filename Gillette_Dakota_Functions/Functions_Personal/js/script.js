4735d90cca1140c0e63391f0f63960ca537a282d
Dakota Gillette <dgillette@fullsail.edu>
2014-10-23 12:52:19 -0700

    prompt validation

--- Gillette_Dakota_Functions/Functions_Personal/js/script.js
+++ Gillette_Dakota_Functions/Functions_Personal/js/script.js
@@ -12,6 +12,12 @@ alert ("We will be asking you questions in order to calculate your water cost/us
 //ask the user their water cost
 var waterCost = prompt("What is you cost of water per 1000 gallons used? The national average is $2 per 1000 gallons. (please enter a number only. example 2.12 )");
 
+//validate the prompt. Checks if it is blank, and if it is a number
+while(isNaN(waterCost) || waterCost==="") {
+	
+	waterCost = prompt("Please enter a number! Do not include the dollar sign. Thanks!");
+}
+
 
 
 
