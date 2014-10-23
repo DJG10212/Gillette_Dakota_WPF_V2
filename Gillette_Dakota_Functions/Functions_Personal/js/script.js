b3c88d7afc1d8fa6498f6a7fe2af02068ff37b2d
Dakota Gillette <dgillette@fullsail.edu>
2014-10-23 12:56:15 -0700

    added validation

--- Gillette_Dakota_Functions/Functions_Personal/js/script.js
+++ Gillette_Dakota_Functions/Functions_Personal/js/script.js
@@ -21,6 +21,13 @@ while(isNaN(waterCost) || waterCost==="") {
 //ask the user their average shower length
 var showerTime = prompt("What is the average amoount of minutes you spend in the shower? ( example: 10 )");
 
+//validate the prompt. Checks if it is blank, and if it is a number
+while(isNaN(showerTime) || showerTime==="") {
+	
+	showerTime = prompt("Please enter a number! Thanks!");
+}
+
+
 
 
 
