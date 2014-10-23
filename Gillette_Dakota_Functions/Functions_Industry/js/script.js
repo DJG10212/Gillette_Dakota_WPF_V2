b03eb684fe0a5a9959417111649b408fc990844a
Dakota Gillette <dgillette@fullsail.edu>
2014-10-23 15:48:00 -0700

    debugging

--- Gillette_Dakota_Functions/Functions_Industry/js/script.js
+++ Gillette_Dakota_Functions/Functions_Industry/js/script.js
@@ -6,44 +6,42 @@ WPF Section Functions Industry
 
 
 //ask the user what state to live in
-var state = prompt("The top 3 average Web Developer salries are in Washington, California, and Maryland. If you were a web developer looking to move to one of these states, which one would you choose?");
+var state = prompt("The top 3 average Web Developer salries are in Washington, California, and Maryland. If you were a web developer looking to move to one of these states, which one would you choose? Please enter 1 for Maryland, 2 for California, 3 for Washington");
 
 //validate the prompt. Checks if it is blank
-while (state === "");  {
-    state = prompt("Please enter either: Washington, California, or Maryland");
-}
+while (isNaN(state) || state === "") {
+    state = prompt("Please enter a number!");
+	}
 
 
-if (state == "Maryland") {
-	
+if (state == 1) {
 	//ask the user their salary
-	var yearlySalary = prompt("Congrats on choosing Maryland! What is your yearly salary? Please enter 		the number only, no commas.");
+	var yearlySalary = prompt("Congrats on choosing Maryland! What is your yearly salary? Please enter the number only, no commas.");
 
 	//validate the prompt. Checks if it is blank, and if it is a number
 	while (isNaN(yearlySalary) || yearlySalary === "") {
-    yearlySalary = prompt("Please enter a number! Do not include the dollar sign or commas. Thanks!");
-	}
+    yearlySalary = prompt("Please enter a number! Do not include the dollar sign or commas. Thanks!");}
 	//average salary in Maryland
 	var marylandAverageSalary = 51860;
 	//average web developer pay in MD
-	var webDevPay = 104934
-	
+    var webDevPay = 104934
 	//defining variables
-	var total = CalcPay (yearlySalary, marylandAverageSalary, webDevPay);
+	var differenceOfPay = CalcPay (yearlySalary, webDevPay);
 	//starting function
-	function calcPay (yearlySalary, marylandAverageSalary, webDevPay){
-		
-
-	
-		
+	function calcPay (yearlySalary, webDevPay){
+		morePay = Number(yearlySalary) - Number(webDevPay);
+			return morePay
 	}
-	
+	//calculate how much more you will make than the average MD worker
+	var moreThan = Number(webDevPay) - Number(marylandAverageSalary);
+alert("If you work as a web developer in Maryland, you will make $" + moreThan + " more than the average yearly salary in Maryland. You will make $" + differenceOfPAy + " more than your current job. What are you waiting for?! Move!!")
 	
 
-}else if (state == "California") {
+
+}else if (state == 2) {
 	
 	//ask the user their salary
-	var yearlySalary = prompt("Congrats on choosing California! What is your yearly salary? Please enter 		the number only, no commas.");
+	var yearlySalary = prompt("Congrats on choosing California! What is your yearly salary? Please enter the number only, no commas.");
 
 	//validate the prompt. Checks if it is blank, and if it is a number
 	while (isNaN(yearlySalary) || yearlySalary === "") {
@@ -53,7 +51,7 @@ if (state == "Maryland") {
 else {
 	
 	//ask the user their salary
-	var yearlySalary = prompt("Congrats on choosing Washington! What is your yearly salary? Please enter 		the number only, no commas.");
+	var yearlySalary = prompt("Congrats on choosing Washington! What is your yearly salary? Please enter the number only, no commas.");
 
 	//validate the prompt. Checks if it is blank, and if it is a number
 	while (isNaN(yearlySalary) || yearlySalary === "") {
