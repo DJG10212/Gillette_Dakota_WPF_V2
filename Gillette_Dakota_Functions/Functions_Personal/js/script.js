d81044fc3cc92bad5509255592ee282ab41f38c1
Dakota Gillette <dgillette@fullsail.edu>
2014-10-23 12:48:20 -0700

    Ask user their water cost

--- Gillette_Dakota_Functions/Functions_Personal/js/script.js
+++ Gillette_Dakota_Functions/Functions_Personal/js/script.js
@@ -9,7 +9,8 @@ Assignment: Functions Personal
 
 alert ("We will be asking you questions in order to calculate your water cost/usage from taking showers.");
 
-
+//ask the user their water cost
+var waterCost = prompt("What is you cost of water per 1000 gallons used? The national average is $2 per 1000 gallons. (please enter a number only. example 2.12 )");
 
 
 
