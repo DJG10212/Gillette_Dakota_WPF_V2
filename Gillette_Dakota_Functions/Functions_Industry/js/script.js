8898594babcf3c53f23b3f466ac8eedbf318a5de
Dakota Gillette <dgillette@fullsail.edu>
2014-10-23 15:22:37 -0700

    working on function

--- Gillette_Dakota_Functions/Functions_Industry/js/script.js
+++ Gillette_Dakota_Functions/Functions_Industry/js/script.js
@@ -5,13 +5,48 @@ WPF Section Functions Industry
 */
 
 
-//ask the user their water cost
+//ask the user what state to live in
 var state = prompt("The top 3 average Web Developer salries are in Washington, California, and Maryland. If you were a web developer looking to move to one of these states, which one would you choose?");
 
-//validate the prompt. Checks if it is blank, and if it is a number
-while (state === "") {
+//validate the prompt. Checks if it is blank
+while (state === "");  {
     state = prompt("Please enter either: Washington, California, or Maryland");
 }
 
 
-if (state == Maryland
+if (state == "Maryland") {
+	
+	//ask the user their salary
+	var yearlySalary = prompt("Congrats on choosing Maryland! What is your yearly salary? Please enter 		the number only, no commas.");
+
+	//validate the prompt. Checks if it is blank, and if it is a number
+	while (isNaN(yearlySalary) || yearlySalary === "") {
+    yearlySalary = prompt("Please enter a number! Do not include the dollar sign or commas. Thanks!");
+	}
+	//average salary in Maryland
+	var marylandSalary = 51860;
+	var total = CalcPay (yearlySalary, marylandSalary);
+	
+
+}else if (state == "California") {
+	
+	//ask the user their salary
+	var yearlySalary = prompt("Congrats on choosing California! What is your yearly salary? Please enter 		the number only, no commas.");
+
+	//validate the prompt. Checks if it is blank, and if it is a number
+	while (isNaN(yearlySalary) || yearlySalary === "") {
+    yearlySalary = prompt("Please enter a number! Do not include the dollar sign or commas. Thanks!");
+	}
+}
+else {
+	
+	//ask the user their salary
+	var yearlySalary = prompt("Congrats on choosing Washington! What is your yearly salary? Please enter 		the number only, no commas.");
+
+	//validate the prompt. Checks if it is blank, and if it is a number
+	while (isNaN(yearlySalary) || yearlySalary === "") {
+    yearlySalary = prompt("Please enter a number! Do not include the dollar sign or commas. Thanks!");
+}
+
+	
+}
